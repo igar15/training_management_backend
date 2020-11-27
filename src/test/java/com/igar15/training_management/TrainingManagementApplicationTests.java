0d49f49dcf376b4814a3acd96642eeee221e8472
Igor Sh <igar15@yandex.ru>
2020-11-27 21:44:58 +0300

    initial commit

---
+++ src/test/java/com/igar15/training_management/TrainingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igar15.training_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
