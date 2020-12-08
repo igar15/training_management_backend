15d5c2b74499f63af54e6d5a97ff9b2ecc782b40
igar15 <igar15@yandex.ru>
2020-12-08 10:34:52 +0300

    Refactor AuthorizationFilter -> after checking authorization token on valid add checking for User is present in database and he is not locked

--- src/main/java/com/igar15/training_management/security/AuthorizationFilter.java
+++ src/main/java/com/igar15/training_management/security/AuthorizationFilter.java
@@ -1,13 +1,18 @@
 package com.igar15.training_management.security;
 
 import com.igar15.training_management.constants.SecurityConstant;
+import com.igar15.training_management.entity.User;
+import com.igar15.training_management.repository.UserRepository;
+import com.igar15.training_management.service.UserService;
 import com.igar15.training_management.utils.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -20,12 +25,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AuthorizationFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -43,10 +52,19 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             String userEmail = jwtTokenProvider.getSubject(token);
 
             if (jwtTokenProvider.isTokenValid(userEmail, token) && SecurityContextHolder.getContext().getAuthentication() == null) {
-                List<GrantedAuthority> authorities = jwtTokenProvider.getAuthoritiesFromToken(token);
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(userEmail, null, authorities);
-                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = null;
+                Optional<User> optionalUser = userRepository.findByEmail(userEmail);
+                if (optionalUser.isPresent()) {
+                    User user = optionalUser.get();
+                    if (!user.isNonLocked()) {
+                        throw new LockedException("User " + userEmail + " account is locked");
+                    }
+//                    List<GrantedAuthority> authorities = jwtTokenProvider.getAuthoritiesFromToken(token);
+                    List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+                    usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(userEmail, null, authorities);
+                    usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                }
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
             else {
