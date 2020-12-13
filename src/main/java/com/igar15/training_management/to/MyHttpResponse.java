31e14001a751a07ebce8f382def4eb3de06d3b55
Igor Sh <igar15@yandex.ru>
2020-12-13 13:33:29 +0300

    Add @Schema examples to MyHttpResponse

--- src/main/java/com/igar15/training_management/to/MyHttpResponse.java
+++ src/main/java/com/igar15/training_management/to/MyHttpResponse.java
@@ -1,17 +1,24 @@
 package com.igar15.training_management.to;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
 import java.util.Date;
 
+@Schema
 public class MyHttpResponse {
 
+    @Schema(example = "200")
     private int httpStatusCode;
+    @Schema(example = "OK")
     private HttpStatus httpStatus;
+    @Schema(example = "OK")
     private String reason;
+    @Schema(example = "Response message")
     private String message;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy HH:mm:ss", timezone = "Europe/Moscow")
+    @Schema(example = "12-15-2020 15:40:25")
     private Date timeStamp;
 
     public MyHttpResponse() {
